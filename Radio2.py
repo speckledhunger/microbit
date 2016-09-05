5e93c60b1e78b0574605299783b1b29cb4b0cc1a
speckledhunger <speckledhunger@users.noreply.github.com>
2016-09-05 15:54:34 +0100

    Add files via upload

---
+++ Radio2.py
@@ -0,0 +1,31 @@
+from microbit import *
+
+
+
+import radio
+
+
+
+radio.on()
+
+
+
+while True:
+
+    if button_a.was_pressed():
+
+        radio.send('bye')
+
+    
+
+    if button_b.was_pressed():
+
+        display.clear()
+        
+    Happy = radio.receive()
+
+    
+
+    if Happy == 'hi':
+
+        display.show(Image.HAPPY)
