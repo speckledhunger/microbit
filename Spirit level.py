8194b8e412c3dab61f4350b89829e0d69402447e
speckledhunger <speckledhunger@users.noreply.github.com>
2016-10-01 20:22:54 +0100

    Update Spirit level.py

--- Spirit level.py
+++ Spirit level.py
@@ -1,10 +1,14 @@
 from microbit import *
 
-while True:
-    reading = accelerometer.get_x()
-    if reading > 20:
-        display.show("]")
-    elif reading < -20:
-        display.show("[")
-    else:
+while True:
+    
+    reading = accelerometer.get_x()
+
+    if reading > 20:
+        display.show("]")
+
+    elif reading < -20:
+        display.show("[")
+
+    else:
         display.show("-")
